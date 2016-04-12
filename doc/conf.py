2e07f3c363a49852115a0893e207009b7b8c838a
Joshua A. Anderson <joaander@umich.edu>
2016-04-12 07:48:24 -0400

    Fix GSD RTD builds

--- doc/conf.py
+++ doc/conf.py
@@ -212,7 +212,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('root', 'GSD.tex', 'GSD Documentation',
+  ('index', 'GSD.tex', 'GSD Documentation',
    'The Regents of the University of Michigan', 'manual'),
 ]
 
