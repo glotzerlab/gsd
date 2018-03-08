919a5b1bc3f36cb8e417d823491a3c0b5a94ea16
Joshua A. Anderson <joaander@umich.edu>
2018-03-08 09:41:25 -0500

    Use althernate method to support clean installs with setup.py
    https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
-try:
-    from numpy import get_include
-except ImportError:
-    def get_include(*args,**kwargs):
-        from numpy import get_include
-        return get_include(*args,**kwargs)
+from setuptools.command.build_ext import build_ext as _build_ext
 import sys
 import hashlib
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 current_checksum = hashlib.sha256()
 with open('gsd/fl.pyx') as f:
@@ -38,10 +41,10 @@ setup(name = 'gsd',
             "Topic :: Scientific/Engineering :: Physics",
         ],
 
-      install_requires=['numpy'],
+      cmdclass={'build_ext':build_ext},
+      setup_requires=['numpy'],
       ext_modules = [Extension('gsd.fl',
-               sources=['gsd/fl{0}.c'.format(sys.version_info.major), 'gsd/gsd.c'],
-               include_dirs = [get_include()]
+               sources=['gsd/fl{0}.c'.format(sys.version_info.major), 'gsd/gsd.c']
                )],
       packages = ['gsd']
      )
