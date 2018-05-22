ff283c2ca04a6a0a605ff20ba42b516d3f374064
Joshua A. Anderson <joaander@umich.edu>
2018-05-22 07:46:59 -0400

    Go back to old setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
-from setuptools.command.build_ext import build_ext as _build_ext
+import numpy
 import sys
 import hashlib
-
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
 
 current_checksum = hashlib.sha256()
 with open('gsd/fl.pyx') as f:
@@ -21,6 +13,11 @@ with open('gsd/fl.pyx.sha256') as f:
 if existing_checksum.strip() != current_checksum.hexdigest():
     print('fl.pyx has been updated, it needs to be cythonized before setup.py can continue.');
     sys.exit(1);
+
+fl = Extension('gsd.fl',
+               sources=['gsd/fl{0}.c'.format(sys.version_info.major), 'gsd/gsd.c'],
+               include_dirs = [numpy.get_include()]
+               )
 
 setup(name = 'gsd',
       version = '1.5.2',
@@ -41,10 +38,7 @@ setup(name = 'gsd',
             "Topic :: Scientific/Engineering :: Physics",
         ],
 
-      cmdclass={'build_ext':build_ext},
-      setup_requires=['numpy'],
-      ext_modules = [Extension('gsd.fl',
-               sources=['gsd/fl{0}.c'.format(sys.version_info.major), 'gsd/gsd.c']
-               )],
+      install_requires=['numpy'],
+      ext_modules = [fl],
       packages = ['gsd']
      )
