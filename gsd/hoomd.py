895574b6798158f2a850227605a0d8c015dad43d
Brandon Butler <butlerbr@umich.edu>
2019-11-04 11:18:56 -0500

    Update __getstate__ docstring for HOOMDTrajectory

--- gsd/hoomd.py
+++ gsd/hoomd.py
@@ -532,7 +532,7 @@ class HOOMDTrajectory(object):
         return self.file.nframes;
 
     def __getstate__(self):
-        '''Checks if in read mode and removes file handle from __dict__'''
+        '''Checks if in read mode and passes necessary reset information'''
         if self.file.mode not in ['rb', 'rb+']:
             raise PickleError("Only trajectories in read only "
                               "mode can be pickled.")
