5ae95e7509ba071adaca107e3004004b320a6cdd
Joshua A. Anderson <joaander@umich.edu>
2020-01-17 09:33:09 -0500

    Additional copyright updates

--- doc/conf.py
+++ doc/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'GSD'
-copyright (c) 2016-2020 The Regents of the University of Michigan'
+copyright = '2016-2020 The Regents of the University of Michigan'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- doc/style.rst
+++ doc/style.rst
@@ -1,3 +1,7 @@
+.. Copyright (c) 2016-2020 The Regents of the University of Michigan
+.. This file is part of the General Simulation Data (GSD) project, released
+.. under the BSD 2-Clause License.
+
 Code style
 ==========
 
