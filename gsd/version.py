01a2eb89b3c76c15a7f1c00b54b377b3b3386dfd
Vyas <vramasub@umich.edu>
2020-06-25 10:08:10 -0400

    Extract version into separate file.

---
+++ gsd/version.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2020 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+
+
+__version__ = "2.1.1"
+
+
+__all__ = [
+    '__version__',
+]
