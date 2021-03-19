57aaf262bac6c591de5f27fb975566bbd9e7bffb
Joshua A. Anderson <joaander@umich.edu>
2021-03-19 09:57:18 -0400

    Add --validate option to pytest
    This shortens the time needed to run tests in default cases.

--- gsd/test/test_largefile.py
+++ gsd/test/test_largefile.py
@@ -10,6 +10,7 @@ import pytest
 import gc
 
 
+@pytest.mark.validate
 @pytest.mark.parametrize("N", [2**27, 2**28, 2**29 + 1])
 def test_large_n(tmp_path, N):
     """Test data chunks and files larger than 2 GB."""
