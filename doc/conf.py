f730137bd1ec57d26bdbe2c97807198a56a7285f
Joshua A. Anderson <joaander@umich.edu>
2023-04-27 13:21:04 -0400

    Fix readthedocs build.

--- doc/conf.py
+++ doc/conf.py
@@ -89,4 +89,4 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-     subprocess.call('cd ../doxygen; doxygen', shell=True)
+     subprocess.call('doxygen', shell=True)
