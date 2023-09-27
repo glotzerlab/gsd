dde2ea1a477b415e6286fdd512bac0d2442e83dd
Joshua A. Anderson <joaander@umich.edu>
2023-09-27 11:47:13 -0400

    Bump version to 3.2.0.

--- gsd/pygsd.py
+++ gsd/pygsd.py
@@ -37,7 +37,7 @@ import struct
 from collections import namedtuple
 import sys
 
-version = "3.1.1"
+version = "3.2.0"
 
 logger = logging.getLogger('gsd.pygsd')
 
--- gsd/version.py
+++ gsd/version.py
@@ -9,7 +9,7 @@ Attributes:
                    not the file layer version it reads/writes.
 """
 
-version = "3.1.1"
+version = "3.2.0"
 
 __all__ = [
     'version',
