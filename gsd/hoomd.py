4b6187841efdba02db10ac2dc1fb575749f5f930
janbridley <bradleyjenj@gmail.com>
2025-10-06 13:59:14 -0400

    Fix typo in doc

--- gsd/hoomd.py
+++ gsd/hoomd.py
@@ -1153,14 +1153,14 @@ def read_log(name: str, scalar_only=False, glob_pattern='*'):
                                                   scalar_only=True))
         df
 
+
     To read only logs from HOOMD-Blue's ``ThermodynamicQuantities``:
 
     .. ipython:: python
 
-        log = gsd.hoomd.read_log('log_example.gsd',
+        log = gsd.hoomd.read_log('log-example.gsd',
                                   glob_pattern="*ThermodynamicQuantities*")
         log.keys()
-
     """
     if not fl_imported:
         msg = 'file layer module is not available'
